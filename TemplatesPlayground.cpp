09c69ec9fe591e3ee2735d872ff066b1c49a11a0
Dappstr <87837549+Dappstr@users.noreply.github.com>
2023-02-25 17:34:19 -0800

    Update TemplatesPlayground.cpp

--- TemplatesPlayground.cpp
+++ TemplatesPlayground.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
+#include <cstring>
+#include <utility>
+#include <algorithm>
+#include <numeric>
 #include <vector>
-#include <cassert>
-#include <iostream>
-#include "Stack1.h"
 
-template <typename T, typename... Ts>
-void someFunc(T& arg, Ts ...args)
+template <typename First, typename... Ts>
+void someFunc(First first, Ts ...args)
 {
-    std::cout << "Number of arguments: " << sizeof...(args) << "\n\n" << "Arguments: " << '\n';
-    ((std::cout << args << '\n'), ...);
+    int arg = first;
 
     ([&arg, &args]
         {
             if constexpr (sizeof...(args) > 0)
             {
-                std::cout << "Increasing x for argument: " << args << '\n';
-                arg++;
+                std::cout << "Increasing arg for argument: " << args << '\n';
+                arg+=args;
             }
         }
     (), ...);
+        
+    std::cout << arg;
+
 }
+
+template <typename T, typename... Ts>
+concept something = requires (T arg, Ts... args) {
+    requires sizeof... (args) > 1;
+    // Needs fixing { (... + args) } noexcept -> std::is_same_v<std::conjunction_v<T, Ts>...>;
+};
+
+template <typename T, typename... Ts,
+typename = std::enable_if_t<
+                            std::conjunction_v<
+                            std::is_same<
+                            std::decay_t<T>, std::decay_t<Ts>>...>>>
+void acc(T first, Ts... args) {
+
+}
+
 
 int main()
 {
-    
-    {
-        static int x{ 0 };
-        std::string s{ "test" };
-
-        someFunc(x, 'a', 1, 2.5, "hello", s);
-
-        std::cout << "\nValue of x: " << x << "\n\n";
-    }
-
-    Stack<int> stackObj{ 2, 3, 4, 5, 6 };
-    std::cout << stackObj;
 
     return 0;
 }
